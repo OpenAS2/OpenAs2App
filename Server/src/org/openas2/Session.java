b40ca67daa53436430e97b957d4413c6959fd301
Christopher Broderick <uhurusurfa@gmail.com>
2016-05-27 10:07:40 -0400

    Change release version number

--- Server/src/org/openas2/Session.java
+++ Server/src/org/openas2/Session.java
@@ -20,7 +20,7 @@ import org.openas2.processor.Processor;
  */
 public interface Session {
     /** Official OpenAS2 release version */
-    public static final String VERSION = "1.3.7";
+    public static final String VERSION = "2.0.0 Alpha 1";
 
     /** Official OpenAS2 title */
     public static final String TITLE = "OpenAS2 v" + VERSION;
