45de9019f39cd6f128dc56380bcf11eab0682dc8
Christopher Broderick <uhurusurfa@gmail.com>
2016-10-09 21:28:11 +0100

    Support restricted HTTP headers and add H2 DB lib into classpath

--- Server/bin/start-openas2.bat
+++ Server/bin/start-openas2.bat
@@ -1,6 +1,13 @@
 @echo off
 rem Purpose:  runs the OpenAS2 application
 
+rem Set some of the base system properties for the Java environment and logging
+rem remove -Dorg.apache.commons.logging.Log=org.openas2.logging.Log if using another logging package
+rem
+set EXTRA_PARMS=-Xms32m -Xmx384m  -Dorg.apache.commons.logging.Log=org.openas2.logging.Log
+rem By default allow restricted HTTP headers
+set EXTRA_PARMS=%EXTRA_PARMS% -Dsun.net.http.allowRestrictedHeaders=true
+
 rem Uncomment any of the following for enhanced debug
 rem set EXTRA_PARMS=%EXTRA_PARMS% -Dmaillogger.debug.enabled=true
 rem set EXTRA_PARMS=%EXTRA_PARMS% -DlogRxdMsgMimeBodyParts=true
@@ -54,11 +61,9 @@ if not "%JAVA%" == "" goto :Check_JAVA_END
     )
     set JAVA=%JAVA_HOME%\bin\java
 :Check_JAVA_END
-
+set LIB_JARS=../lib/h2-1.4.192.jar;../lib/javax.mail.jar;../lib/bcpkix-jdk15on-154.jar;../lib/bcprov-jdk15on-154.jar;../lib/bcmail-jdk15on-154.jar;../lib/commons-logging-1.2.jar;../lib/openas2-server.jar
 rem    
-rem remove -Dorg.apache.commons.logging.Log=org.openas2.logging.Log if using another logging package
-rem
-"%JAVA%" "%EXTRA_PARMS% -Xms32m -Xmx384m  -Dorg.apache.commons.logging.Log=org.openas2.logging.Log  -cp .;../lib/javax.mail.jar;../lib/bcpkix-jdk15on-154.jar;../lib/bcprov-jdk15on-154.jar;../lib/bcmail-jdk15on-154.jar;../lib/commons-logging-1.2.jar;../lib/openas2-server.jar org.openas2.app.OpenAS2Server ../config/config.xml
+"%JAVA%" %EXTRA_PARMS%  -cp .;%LIB_JARS% org.openas2.app.OpenAS2Server ../config/config.xml
 
 :warn
 :END
