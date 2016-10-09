bdf0aebb9551ee0a0ec893bc87d1986c4f935ccf
Christopher Broderick <uhurusurfa@gmail.com>
2016-10-09 14:54:09 +0100

    Updated to support running the java program as a daemon and by default allow restricted HTRP headers

--- Server/bin/start-openas2.sh
+++ Server/bin/start-openas2.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # purpose: runs the OpenAS2 application     
 x=`basename $0`
 
+binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 keyStorePwd=$1
 PWD_OVERRIDE=""
+
+# Set some of the base system properties for the Java environment and logging
+# remove -Dorg.apache.commons.logging.Log=org.openas2.logging.Log if using another logging package    
+#
+EXTRA_PARMS="-Xms32m -Xmx384m -Dorg.apache.commons.logging.Log=org.openas2.logging.Log"
+# By default allow restricted HTTP headers
+EXTRA_PARMS="$EXTRA_PARMS -Dsun.net.http.allowRestrictedHeaders=true"
 # Uncomment any of the following for enhanced debug
 #EXTRA_PARMS="$EXTRA_PARMS -Dmaillogger.debug.enabled=true"
 #EXTRA_PARMS="$EXTRA_PARMS -DlogRxdMsgMimeBodyParts=true"
 #EXTRA_PARMS="$EXTRA_PARMS -DlogRxdMdnMimeBodyParts=true"
+#EXTRA_PARMS="$EXTRA_PARMS -Djavax.net.debug=SSL"
 
 if [  ! -z $keyStorePwd ]; then
   PWD_OVERRIDE="-Dorg.openas2.cert.Password=$keyStorePwd"
@@ -29,10 +38,16 @@ fi
 
 if [ -z $JAVA_HOME ]; then
   echo "ERROR: Cannot find JAVA_HOME"
-  exit
+  exit 1
 fi
+
+LIB_JARS="${binDir}/../lib/h2-1.4.192.jar:${binDir}/../lib/javax.mail.jar:${binDir}/../lib/bcpkix-jdk15on-154.jar:${binDir}/../lib/bcprov-jdk15on-154.jar:${binDir}/../lib/bcmail-jdk15on-154.jar:${binDir}/../lib/commons-logging-1.2.jar:${binDir}/../lib/openas2-server.jar"
 JAVA_EXE=$JAVA_HOME/bin/java 
 #    
-# remove -Dorg.apache.commons.logging.Log=org.openas2.logging.Log if using another logging package    
-#
-$JAVA_EXE ${PWD_OVERRIDE} -Xms32m -Xmx384m -Dorg.apache.commons.logging.Log=org.openas2.logging.Log  -cp .:../lib/javax.mail.jar:../lib/bcpkix-jdk15on-154.jar:../lib/bcprov-jdk15on-154.jar:../lib/bcmail-jdk15on-154.jar:../lib/commons-logging-1.2.jar:../lib/openas2-server.jar org.openas2.app.OpenAS2Server ../config/config.xml
+CMD="$JAVA_EXE ${PWD_OVERRIDE} ${EXTRA_PARMS}  -cp .:${LIB_JARS}  org.openas2.app.OpenAS2Server ${binDir}/../config/config.xml"
+if [ "true" = "$OPENAS2_AS_DAEMON" ]; then
+  $CMD &
+else
+  $CMD
+fi
+exit $?
