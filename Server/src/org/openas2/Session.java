e7f0141cde64c6311aff2e2ae0eeb8c937a2ac3f
Christopher Broderick <uhurusurfa@gmail.com>
2016-10-17 16:34:44 +0100

    Change version number for bugfix release

--- Server/src/org/openas2/Session.java
+++ Server/src/org/openas2/Session.java
@@ -20,7 +20,7 @@ import org.openas2.processor.Processor;
  */
 public interface Session {
     /** Official OpenAS2 release version */
-    public static final String VERSION = "2.1.0";
+    public static final String VERSION = "2.1.1";
 
     /** Official OpenAS2 title */
     public static final String TITLE = "OpenAS2 v" + VERSION;
