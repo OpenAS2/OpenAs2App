6f1b5e2bccd254efd0e5a735312120e58763a9cf
Christopher Broderick <uhurusurfa@gmail.com>
2017-01-13 10:39:20 +0000

    Initial basic Activator class for running OpenAS2 as an OSGi bundle

--- Bundle/src/main/java/org/openas2/osgi/Activator.java
+++ Bundle/src/main/java/org/openas2/osgi/Activator.java
@@ -1,11 +1,14 @@
 package org.openas2.osgi;
 
+import org.openas2.app.OpenAS2Server;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	private ServiceRegistration<?> openAS2Registration;
 
 	static BundleContext getContext() {
 		return context;
@@ -17,6 +20,9 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		OpenAS2Server openAS2Service = new OpenAS2Server();
+		openAS2Registration = bundleContext.registerService(OpenAS2Server.class.getName(), openAS2Service, null);
+		openAS2Service.start(null);
 	}
 
 	/*
@@ -24,6 +30,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		openAS2Registration.unregister();
 		Activator.context = null;
 	}
 
