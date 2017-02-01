631b3291e91cdc3b0029d3798f7ba90a1bad8b17
Christopher Broderick <uhurusurfa@gmail.com>
2017-02-01 15:25:10 +0000

    Support PID from a file for starting and stopping

--- Server/bin/openas2.d
+++ Server/bin/openas2.d
@@ -6,7 +6,7 @@
 # Required-Stop:     
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Startup script to launch OpenAS2 appliction as a daemon
+# Short-Description: Startup script to launch OpenAS2 application as a daemon
 # Description:       This script can be used in any NIX based system that implements the init.d mechanism
 #					 The EXECUTABLE variable below must be set to point to the OpenAS2 startup script
 #                    See the OpenAS2HowTo.pdf for details on configuration checks for this mode of running OpenAS2
@@ -14,13 +14,26 @@
 
 SERVICE_NAME=OpenAS2
 EXECUTABLE=/opt/OpenAS2/bin/start-openas2.sh
+export PID_FILE=/opt/OpenAS2/bin/OpenAS2.pid
 THIS_SCRIPT_NAME=`basename $0`
 THIS_SCRIPT_EXEC=$0
 if [ "$THIS_SCRIPT_NAME" = "$0" ]; then
   THIS_SCRIPT_EXEC="/etc/init.d/$0"
 fi
-PID=$(ps -ef | grep java | grep org.openas2.app.OpenAS2Server | awk '{print $2}')
 
+PID=""
+PID_FILE=/opt/OpenAS2/bin/OpenAS2.pid
+#PID=$(ps -ef | grep java | grep org.openas2.app.OpenAS2Server | awk '{print $2}')
+if [ -f $PID_FILE ]; then
+  PID=`cat $PID_FILE`
+fi
+if [ ! -z $PID ]; then
+  x=$(ps -p $PID  2>/dev/null)
+  if [ "$?" = 1 ]; then
+    PID=""
+    echo "" > $PID_FILE
+  fi
+fi
 case "$1" in
     start)
         echo "Starting $SERVICE_NAME ..."
@@ -44,6 +57,7 @@ case "$1" in
           echo "Attempting to stop $SERVICE_NAME..."
           kill $PID
           if [ "$?" = 0 ]; then
+            echo "" > $PID_FILE
             echo "$SERVICE_NAME terminated ..."
             exit 0
           else
@@ -60,6 +74,7 @@ case "$1" in
           echo "Attempting to force termination of $SERVICE_NAME..."
           kill -9 $PID
           if [ "$?" = 0 ]; then
+            echo "" > $PID_FILE
             echo "$SERVICE_NAME terminated ..."
             exit 0
           else
@@ -90,6 +105,7 @@ case "$1" in
             echo "ERROR: Failed to stop $SERVICE_NAME"
             exit 1
           else
+            echo "" > $PID_FILE
             PID=""
           fi
         fi
