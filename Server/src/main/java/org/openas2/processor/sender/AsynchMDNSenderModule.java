350ba0f13c575711baf4ed773844e1cff9001159
Christopher Broderick <uhurusurfa@gmail.com>
2017-05-02 11:33:07 +0100

    Support ASYNC MDN send retry

--- Server/src/main/java/org/openas2/processor/sender/AsynchMDNSenderModule.java
+++ Server/src/main/java/org/openas2/processor/sender/AsynchMDNSenderModule.java
@@ -20,6 +20,7 @@ import org.openas2.message.Message;
 import org.openas2.message.MessageMDN;
 import org.openas2.processor.resender.ResenderModule;
 import org.openas2.processor.storage.StorageModule;
+import org.openas2.util.AS2Util;
 import org.openas2.util.DateUtil;
 import org.openas2.util.DispositionType;
 import org.openas2.util.IOUtilOld;
@@ -94,6 +95,7 @@ public class AsynchMDNSenderModule extends HttpSenderModule {
 				conn.setRequestProperty("Connection", "close, TE");
 				conn.setRequestProperty("User-Agent", msg.getAppTitle() + " (AsyncMDNSenderModule)");
 				// Copy all the header from mdn to the RequestProperties of conn
+				@SuppressWarnings("unchecked")
 				Enumeration<Header> headers = mdn.getHeaders().getAllHeaders();
 				Header header = null;
 				while (headers.hasMoreElements()) {
@@ -189,9 +191,37 @@ public class AsynchMDNSenderModule extends HttpSenderModule {
 
 	protected void resend(Message msg, OpenAS2Exception cause)
 			throws OpenAS2Exception {
+        // Get the resend retry count
+		Map<Object, Object> msgOptions = msg.getOptions();
+        String tries = AS2Util.retries(msgOptions, getParameter(SenderModule.SOPT_RETRIES, false));
+        if (logger.isDebugEnabled())
+        	logger.debug("MDN resend retries: MSG - " + msgOptions.get(SenderModule.SOPT_RETRIES) + "   ::: RETRIES - " + tries);
+		int retries = -1;
+		if (tries == null) tries = SenderModule.DEFAULT_RETRIES;
+		try {
+			retries = Integer.parseInt(tries);
+		} catch (Exception e) {
+			msg.setLogMsg("The retry count is not a valid integer value: " + tries);
+			logger.error(msg);
+		}
+		if (msgOptions.get(SenderModule.SOPT_RETRIES) == null)
+			msgOptions.put(SenderModule.SOPT_RETRIES, retries);
+		if (logger.isTraceEnabled()) logger.trace("Send MDN retry count: " + retries);
+    	if (retries >= 0 && retries -- <= 0)
+    	{
+    		msg.setLogMsg("MDN response abandoned after retry limit reached.");
+        	logger.error(msg);
+        	// Log significant msg state
+            msg.setOption("STATE", Message.MSG_STATE_MSG_RXD_MDN_SENDING_FAIL);
+            msg.trackMsgState(getSession());
+            AS2Util.cleanupFiles(msg, false);
+    		throw new OpenAS2Exception("MDN response abandoned after retry limit reached." + msg.getLogMsgID());
+    	}
 		Map<Object, Object> options = new HashMap<Object, Object>();
 		options.put(ResenderModule.OPTION_CAUSE, cause);
 		options.put(ResenderModule.OPTION_INITIAL_SENDER, this);
+        options.put(ResenderModule.OPTION_RESEND_METHOD, SenderModule.DO_SENDMDN);
+        options.put(ResenderModule.OPTION_RETRIES, "" + retries);
 		getSession().getProcessor().handle(ResenderModule.DO_RESEND, msg, options);
 	}
 
