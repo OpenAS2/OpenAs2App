db2578d4f970e82b53e76515bf6e8da8a4a201f1
Christopher Broderick <uhurusurfa@gmail.com>
2017-05-02 11:36:09 +0100

    Avoid changing property names to lowercase when pushed into Properties object

--- Server/src/main/java/org/openas2/XMLSession.java
+++ Server/src/main/java/org/openas2/XMLSession.java
@@ -139,7 +139,7 @@ public class XMLSession extends BaseSession {
 
     private void loadProperties(Node propNode)
     {
-        Map<String, String> properties = XMLUtil.mapAttributes(propNode);
+        Map<String, String> properties = XMLUtil.mapAttributes(propNode, false);
         // Make key things accessible via static object for things that do not have accesss to session object
         properties.put(Properties.APP_TITLE_PROP, getAppTitle());
         properties.put(Properties.APP_VERSION_PROP, getAppVersion());
