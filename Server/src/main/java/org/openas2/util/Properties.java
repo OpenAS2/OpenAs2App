5e45e104e848d4ec1ae5c1ce75742e6d29afc425
Christopher Broderick <uhurusurfa@gmail.com>
2018-04-17 23:31:54 +0100

    Add property setter method

--- Server/src/main/java/org/openas2/util/Properties.java
+++ Server/src/main/java/org/openas2/util/Properties.java
@@ -7,12 +7,18 @@ public class Properties
 {
 	public static String APP_VERSION_PROP = "app.version";
 	public static String APP_TITLE_PROP = "app.title";
+	public static String APP_BASE_DIR_PROP = "app.base.dir";
 	
 	private static Map<String, String> _properties = new HashMap<String, String>();
 	
 	public static void setProperties(Map<String, String> map)
 	{
 		_properties = map;
+	}
+
+	public static void setProperty(String prop, String val)
+	{
+		_properties.put(prop, val);
 	}
 
 	public static Map<String, String> getProperties()
