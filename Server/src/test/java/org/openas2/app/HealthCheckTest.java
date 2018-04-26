7e1232b21ce6aad1d87058da88e9adb5f9cc3b4d
Christopher Broderick <uhurusurfa@gmail.com>
2018-04-26 02:38:08 +0100

    Changes to support more re-usable healthcheck and add unit test

---
+++ Server/src/test/java/org/openas2/app/HealthCheckTest.java
@@ -0,0 +1,63 @@
+package org.openas2.app;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.openas2.TestResource;
+import org.openas2.processor.ActiveModule;
+import org.openas2.processor.receiver.NetModule;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class HealthCheckTest {
+    private static final TestResource RESOURCE = TestResource.forGroup("SingleServerTest");
+    //private static File openAS2AHome;
+    private static OpenAS2Server openAS2A;
+    private static ExecutorService executorService;
+
+    @Rule
+    public TemporaryFolder tmp = new TemporaryFolder();
+
+    @BeforeClass
+    public static void startServer() throws Exception
+    {
+        executorService = Executors.newFixedThreadPool(20);
+        System.setProperty("org.apache.commons.logging.Log", "org.openas2.logging.Log");
+        //System.setProperty("org.openas2.logging.defaultlog", "TRACE");
+
+        //openAS2AHome = RESOURCE.get("OpenAS2A");
+        HealthCheckTest.openAS2A = new OpenAS2Server.Builder().run(RESOURCE.get("OpenAS2A", "config", "config.xml").getAbsolutePath());
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception
+    {
+        openAS2A.shutdown();
+        executorService.shutdown();
+    }
+
+    @Test
+    public void shouldRespondToHealthCheckOnNetModule() throws Exception
+    {
+	Class<NetModule> listenerClass = NetModule.class;
+        List<ActiveModule> listeners = openAS2A.getSession().getProcessor().getActiveModulesByClass(listenerClass);
+	for (ActiveModule listener : listeners)
+	{
+	    List<String> failures = new ArrayList<String> ();
+	    NetModule m =(NetModule) listener;
+	    assertThat("Verify healthcheck URI is working on active NetModule instance", m.healthcheck(failures), is(true));
+	}
+    }
+
+
+}
