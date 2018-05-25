878dc7bc7f2a70c24a4645d093f68a4a4c28ae4b
chris <chris@Ubuntu16>
2018-05-25 16:35:26 +0100

    New email helper class

---
+++ Server/src/main/java/org/openas2/mail/ServerParameters.java
@@ -0,0 +1,42 @@
+package org.openas2.mail;
+
+
+class ServerParameters {
+	private String userId;
+	private String password;
+	private String hostName;
+	private String port;
+	private String mailProtocol;
+	private String javaxMailPropFile;
+
+	public String getJavaxMailPropFile() {
+		return javaxMailPropFile;
+	}
+	public ServerParameters setJavaxMailPropFile(String javaxMailPropFile) {
+		this.javaxMailPropFile = javaxMailPropFile;
+		return this;
+	}
+
+	public String getSmtpServer() {
+		return hostName;
+	}
+	public ServerParameters setSmtpServer(String smtpServer) {
+		this.hostName = smtpServer;
+		return this;
+	}
+	public String getSmtpPort() {
+		return port;
+	}
+	public ServerParameters setSmtpPort(String smtpPort) {
+		this.port = smtpPort;
+		return this;
+	}
+	public String getSmtpProtocol() {
+		return mailProtocol;
+	}
+	public ServerParameters setSmtpProtocol(String smtpProtocol) {
+		this.mailProtocol = smtpProtocol;
+		return this;
+	}
+}
+
