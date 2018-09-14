9a226f9d7a83fbde2255a89cbf02cf4c077204b1
Jan Šmucr <jena.s@centrum.cz>
2018-09-14 07:37:47 +0200

    Consider non base64 encodings being rather binary
    The previous version presumes that 7-bit, 8-bit or even quoted-printable encodings can be handled using base64 decoders which is obviously not true and it's better to handle these as binary.

--- Server/src/main/java/org/openas2/lib/helper/BCCryptoHelper.java
+++ Server/src/main/java/org/openas2/lib/helper/BCCryptoHelper.java
@@ -529,7 +529,7 @@ public class BCCryptoHelper implements ICryptoHelper {
     protected String getEncoding(String contentTxfrEncoding)
     {
         // Bouncy castle only deals with binary or base64 so pass base64 for 7bit, 8bit etc
-        return "binary".equalsIgnoreCase(contentTxfrEncoding) ? "binary" : "base64";
+        return "base64".equalsIgnoreCase(contentTxfrEncoding) ? "base64" : "binary";
     }
 
     protected X509Certificate castCertificate(Certificate cert) throws GeneralSecurityException
