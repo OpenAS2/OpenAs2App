0b9727ef54fa0d2ee1f5cbe30b5e0deb0a774c2c
Christopher Broderick <uhurusurfa@gmail.com>
2021-10-25 08:26:00 +0100

    Fix the broken parts of the script.

--- Server/src/bin/gen_p12_key_par.bat
+++ Server/src/bin/gen_p12_key_par.bat
@@ -7,58 +7,11 @@ set sigAlg="%3%withRSA"
 
 set dName=%4%
 
-echo DNAM = %dName%
-
-set CertValidDays=3650
+set CertValidDays=2900
 
 rem Setup the Java Virtual Machine
-if not "%JAVA%" == "" goto :Check_JAVA_END
-    if not "%JAVA_HOME%" == "" goto :TryJDKEnd
-        call :warn JAVA_HOME not set; results may vary
-:TryWOWJRE
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\WOW6432NODE\JavaSoft\Java Runtime Environment" /s /v CurrentVersion ^| find "CurrentVersion"`) DO (
-       set JAVA_VERSION=%%A
-    )
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\WOW6432NODE\JavaSoft\Java Runtime Environment\%JAVA_VERSION%" /s /v JavaHome ^| find "JavaHome"`) DO (
-       set JAVA_HOME=%%A %%B
-    )
-    if not exist "%JAVA_HOME%" goto :TryWOWJDK
-    goto TryJDKEnd
-:TryWOWJDK
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\WOW6432NODE\JavaSoft\Java Development Kit" /s /v CurrentVersion  ^| find "CurrentVersion"`) DO (
-       set JAVA_VERSION=%%A
-    )
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\WOW6432NODE\JavaSoft\Java Development Kit\%JAVA_VERSION%" /s /v JavaHome ^| find "JavaHome"`) DO (
-       set JAVA_HOME=%%A %%B
-    )
-    if not exist "%JAVA_HOME%" goto :TryJRE
-    goto TryJDKEnd
-:TryJRE
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment" /s /v CurrentVersion  ^| find "CurrentVersion"`) DO (
-       set JAVA_VERSION=%%A
-    )
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment\%JAVA_VERSION%" /s /v JavaHome ^| find "JavaHome"`) DO (
-       set JAVA_HOME=%%A %%B
-    )
-    if not exist "%JAVA_HOME%" goto :TryJDK
-    goto TryJDKEnd
-:TryJDK
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\JavaSoft\Java Development Kit" /s /v CurrentVersion  ^| find "CurrentVersion"`) DO (
-       set JAVA_VERSION=%%A
-    )
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKLM\Software\JavaSoft\Java Development Kit\%JAVA_VERSION%" /s /v JavaHome ^| find "JavaHome"`) DO (
-       set JAVA_HOME=%%A %%B
-    )
-    if not exist "%JAVA_HOME%" (
-       call :warn Unable to retrieve JAVA_HOME from Registry
-    )
-:TryJDKEnd
-    if not exist "%JAVA_HOME%" (
-        call :warn JAVA_HOME is not valid: "%JAVA_HOME%"
-        goto END
-    )
-    set JAVA=%JAVA_HOME%\bin\java
-:Check_JAVA_END
+call "%OPENAS2_BASE_DIR%\bin\find_java.bat"
+if %ERRORLEVEL% NEQ 0 exit /B 1
 
 echo        Generate a certificate to a PKCS12 key store.
 echo        Generating certificate:  using alias %certAlias% to %tgtStore%.p12"
@@ -90,15 +43,15 @@ echo.
 goto :END
 
 :Usage
-    echo maGenerate a certificate to a PKCS12 key store.
+  echo Generate a certificate to a PKCS12 key store.
   echo You must supply a target key store without the extension (extension will be added as .p12) and an alias for generated certificate.
-  echo usage: gen_p12_key_par <target keystore> <cert alias> <sigalg> <distinguished name>
+  echo usage: gen_p12_key_par ^<target keystore^> ^<cert alias^> ^<sigalg^> ^<distinguished name^>
   echo             WHERE
   echo                target keystore = name of the target keystore file without .p12 extension
   echo                cert alias = alias name for the digital certificate
   echo                sigalg = signing algorithm for the digital certificate ... SHA256, MD5 etc
   echo                distinguished name = a string in the format:
-  echo                                        CN=<cName>, OU=<orgUnit>, O=<org>, L=<city>, S=<state>, C=<countryCode>
+  echo                                        CN=^<cName^>, OU=^<orgUnit^>, O=^<org^>, L=^<city^>, S=^<state^>, C=^<countryCode^>
 
   echo. 
   echo        eg. gen_p12_key_par as2_certs partnera SHA256 "CN=PartnerA Testing, OU=QA, O=PartnerA, L=New York, S=New York, C=US"
