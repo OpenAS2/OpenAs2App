b2e008cd69156449ca4b0d592845ddf6ea197ca8
Christopher Broderick <uhurusurfa@gmail.com>
2022-06-04 23:38:21 +0100

    Move control parameters into base class for better usability. Support NOT replacing param if not found.

--- Server/src/main/java/org/openas2/params/CompositeParameters.java
+++ Server/src/main/java/org/openas2/params/CompositeParameters.java
@@ -12,31 +12,23 @@ import java.util.StringTokenizer;
 
 public class CompositeParameters extends ParameterParser {
     private Map<String, ParameterParser> parameterParsers;
-    private boolean ignoreMissingParsers;
+
     private Log logger = LogFactory.getLog(CompositeParameters.class.getSimpleName());
 
-    public CompositeParameters(boolean ignoreMissingParsers) {
+    public CompositeParameters(boolean returnEmptyStringForMissingParsers) {
         super();
-        this.ignoreMissingParsers = ignoreMissingParsers;
+        setReturnEmptyStringForMissingParsers(returnEmptyStringForMissingParsers);
     }
 
-    public CompositeParameters(boolean ignoreMissingParsers, Map<String, ParameterParser> parameterParsers) {
+    public CompositeParameters(boolean returnEmptyStringForMissingParsers, Map<String, ParameterParser> parameterParsers) {
         super();
-        this.ignoreMissingParsers = ignoreMissingParsers;
+        setReturnEmptyStringForMissingParsers(returnEmptyStringForMissingParsers);
         getParameterParsers().putAll(parameterParsers);
     }
 
     public CompositeParameters add(String key, ParameterParser param) {
         getParameterParsers().put(key, param);
         return this;
-    }
-
-    public void setIgnoreMissingParsers(boolean ignoreMissingParsers) {
-        this.ignoreMissingParsers = ignoreMissingParsers;
-    }
-
-    public boolean getIgnoreMissingParsers() {
-        return ignoreMissingParsers;
     }
 
     public void setParameter(String key, String value) throws InvalidParameterException {
@@ -58,7 +50,7 @@ public class CompositeParameters extends ParameterParser {
             }
 
             parser.setParameter(keyBuf.toString(), value);
-        } else if (!getIgnoreMissingParsers()) {
+        } else if (!getReturnEmptyStringForMissingParsers()) {
             if (logger.isDebugEnabled()) {
                 logger.debug("Failed to find a parser for: " + key + "  ::: Parser list: " + getParameterParsers().keySet().toString());
             }
@@ -97,7 +89,7 @@ public class CompositeParameters extends ParameterParser {
             }
 
             return parser.getParameter(keyBuf.toString());
-        } else if (!getIgnoreMissingParsers()) {
+        } else if (!getReturnEmptyStringForMissingParsers()) {
             if (logger.isInfoEnabled()) {
                 logger.info("Failed to find a parser for: " + key + "  ::: Available parser list: " + getParameterParsers().keySet().toString());
             }
