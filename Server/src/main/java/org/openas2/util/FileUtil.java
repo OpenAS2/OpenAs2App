7182e9e2acf4bdd62fe42d022db5597b7aad820a
Christopher Broderick <uhurusurfa@gmail.com>
2023-11-07 07:59:56 +0000

    Helper methods for file related stuff

--- Server/src/main/java/org/openas2/util/FileUtil.java
+++ Server/src/main/java/org/openas2/util/FileUtil.java
@@ -5,15 +5,45 @@ import org.openas2.OpenAS2Exception;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 
 public class FileUtil {
 
     //private static final Log logger = LogFactory.getLog(FileUtil.class.getSimpleName());
+
+    public static Properties loadProperties(String filename) throws IOException {
+        Properties fileProps = new java.util.Properties();
+        FileInputStream fis = null;
+        fis = new FileInputStream(filename);
+        try {
+            fileProps.load(fis);
+        } finally {
+            if (fis != null) {
+                    fis.close();
+            }
+        }
+        return fileProps;
+    }
+
+    /** Attempts to extract the filename extension by searching for the last occurrence
+     *  of a period and returning all characters following that period.
+     *  If no period is found then it returns null.
+     * @param filename - the full name of the file including extension
+     * @return the extension of the filename excluding the period
+     */
+    public static String getFilenameExtension(String filename) {
+        int period_index = filename.lastIndexOf(".");
+        if (period_index == -1) {
+            return null;
+        }
+        return filename.substring(filename.lastIndexOf(".") + 1);
+    }
 
     public static void splitLineBasedFile(File sourceFile, String outputDir, long maxFileSize, boolean containsHeaderRow, String newFileBaseName, String filenamePrefix) throws OpenAS2Exception {
         FileReader fileReader;
