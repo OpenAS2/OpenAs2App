fabd483466626836ed0c3ae57fab9052ef2452a8
Christopher Broderick <uhurusurfa@gmail.com>
2024-10-08 16:47:15 +0100

    Add the header

--- Server/src/main/java/org/openas2/processor/receiver/HealthCheckModule.java
+++ Server/src/main/java/org/openas2/processor/receiver/HealthCheckModule.java
@@ -1,3 +1,6 @@
+/* Copyright Uhuru Technology 2016 https://www.uhurutechnology.com
+ * Distributed under the GPLv3 license or a commercial license must be acquired.
+ */
 package org.openas2.processor.receiver;
 
 import java.util.List;
