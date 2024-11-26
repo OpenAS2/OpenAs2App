5e82422b9ae83c6df50c05b2711ac002f347cbbc
Christopher Broderick <uhurusurfa@gmail.com>
2024-11-26 15:28:39 +0000

    Add identifiers for certificate factories.

--- Server/src/main/java/org/openas2/cert/CertificateFactory.java
+++ Server/src/main/java/org/openas2/cert/CertificateFactory.java
@@ -2,14 +2,18 @@ package org.openas2.cert;
 
 import org.openas2.Component;
 import org.openas2.OpenAS2Exception;
+import org.openas2.params.InvalidParameterException;
 
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
 public interface CertificateFactory extends Component {
-    String COMPID_CERTIFICATE_FACTORY = "certificatefactory";
+    String COMPID_AS2_CERTIFICATE_FACTORY = "as2_certs";
+    String COMPID_SSL_TRUST_CERTIFICATE_FACTORY = "ssl_trust_certs";
 
     PrivateKey getPrivateKey(String alias) throws OpenAS2Exception;
 
     X509Certificate getCertificate(String alias) throws OpenAS2Exception;
+
+    String getIdentifier() throws InvalidParameterException;
 }
