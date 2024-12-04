c06cab4cdfe36a4d4df2b2b158bbea02b1fffa35
Christopher Broderick <uhurusurfa@gmail.com>
2024-12-04 03:33:18 +0000

    Enhancements to support extended testing

--- Server/src/test/java/org/openas2/app/BaseServerSetup.java
+++ Server/src/test/java/org/openas2/app/BaseServerSetup.java
@@ -1,16 +1,20 @@
 package org.openas2.app;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.io.TempDir;
 import org.openas2.TestResource;
 import org.openas2.XMLSession;
 import org.openas2.message.AS2Message;
+import org.openas2.message.FileAttribute;
 import org.openas2.message.Message;
 import org.openas2.partner.Partnership;
+import org.openas2.processor.receiver.api.AS2FileReceiverModule;
 import org.openas2.util.Properties;
 
 
@@ -18,10 +22,10 @@ public class BaseServerSetup {
     private static final TestResource RESOURCE = TestResource.forGroup("SingleServerTest");
     static String myCompanyOid = "MyCompany_OID";
     static String myPartnerOid = "PartnerA_OID";
+    private boolean startActiveModules = false;
 
-    // private static File openAS2AHome;
     protected static XMLSession session;
-    protected static Message msg;
+    protected static Message simpleTestMsg;
 
     @TempDir
     public static File tmpDir;
@@ -32,6 +36,31 @@ public class BaseServerSetup {
         setup();
     }
 
+    public AS2Message  getSimpleTestMsg() throws Exception {
+        AS2Message msg = new AS2Message();
+        Partnership myPartnership = msg.getPartnership();
+        myPartnership.setSenderID(Partnership.PID_AS2, myCompanyOid);
+        myPartnership.setReceiverID(Partnership.PID_AS2, myPartnerOid);
+        myPartnership.setSenderID(Partnership.PID_AS2, myCompanyOid);
+        session.getPartnershipFactory().updatePartnership(msg, true);
+        return msg;
+    }
+
+    public void addSendPayloadStuffToMsg(String fileName, Message msg) throws Exception {
+        AS2FileReceiverModule frm = new AS2FileReceiverModule();
+        msg.setAttribute(FileAttribute.MA_FILENAME, fileName);
+        msg.setHeader("AS2-To", msg.getPartnership().getReceiverID(Partnership.PID_AS2));
+        msg.setHeader("AS2-From", msg.getPartnership().getSenderID(Partnership.PID_AS2));
+        msg.updateMessageID();
+        File testFile = new File(tmpDir, fileName);
+        FileUtils.writeStringToFile(testFile, "Show me the money!", Charset.forName("UTF-8"));
+        frm.buildMessageData(msg, testFile, null);
+    }
+
+    public void setStartActiveModules(boolean startActiveModules) {
+        this.startActiveModules = startActiveModules;
+    }
+
     public void createFileSystemResources() throws Exception {
         tmpDir = Files.createTempDirectory("testResources").toFile();
         openAS2PropertiesFile = new File(tmpDir, "test.openas2.properties");
@@ -39,17 +68,15 @@ public class BaseServerSetup {
 
     public void setup() throws Exception {
         try {
-            System.setProperty("OPENAS2_LOG_LEVEL", "trace");
+            //System.setProperty("OPENAS2_LOG_LEVEL", "trace");
             if (openAS2PropertiesFile.exists()) {
                 System.setProperty(Properties.OPENAS2_PROPERTIES_FILE_PROP, openAS2PropertiesFile.getAbsolutePath());
             }
             session = new XMLSession(RESOURCE.get("MyCompany", "config", "config.xml").getAbsolutePath());
-            msg = new AS2Message();
-            Partnership myPartnership = msg.getPartnership();
-            myPartnership.setSenderID(Partnership.PID_AS2, myCompanyOid);
-            myPartnership.setReceiverID(Partnership.PID_AS2, myPartnerOid);
-            myPartnership.setSenderID(Partnership.PID_AS2, myCompanyOid);
-            session.getPartnershipFactory().updatePartnership(msg, true);
+            simpleTestMsg = getSimpleTestMsg();
+            if (startActiveModules) {
+                session.start();
+            }
         } catch (Throwable e) {
             // aid for debugging JUnit tests
             System.err.println("ERROR occurred: " + ExceptionUtils.getStackTrace(e));
